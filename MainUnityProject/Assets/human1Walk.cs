2cc3f613c3d0a7dfa7174e3f98e191afbb82e368
Lyd <teknisk+lyd-GDC2025-GR3@game.unf.dk>
2025-07-16 22:30:48 +0200

    allot

---
+++ MainUnityProject/Assets/human1Walk.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class human1Walk : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public float range;
+    
+    public Transform centrePoint;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            Vector3 point;
+            if (RandomPoint(centrePoint.position, range, out point))
+            {
+                Debug.DrawRay(point, Vector3.up,Color.blue,1.0f);
+                agent.SetDestination(point);
+            }
+
+        }
+    }
+
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+        Vector3 randomPoint = center + Random.insideUnitSphere * range;
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+        {
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
+    }
+}
