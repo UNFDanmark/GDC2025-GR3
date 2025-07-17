97f5771fb9bcd09fa2752a47313c249284548137
Programmering <teknisk+programmering-GDC2025-GR3@game.unf.dk>
2025-07-17 09:14:16 +0200

    sceneupdater thingy

--- MainUnityProject/Assets/enternewscreen.cs
+++ MainUnityProject/Assets/enternewscreen.cs
@@ -1,37 +1,48 @@
 using System;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Enternewscreen : MonoBehaviour
 {
     PlayerScript pSS;
     public GameObject deathscreen;
     public GameObject winscreen;
+        
+    bool isdead;
     // private MonoBehaviour scriptToDisable;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        isdead = false;
         pSS = GameObject.FindWithTag("GameController").GetComponent<PlayerScript>();
         winscreen.SetActive(false);
         deathscreen.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if (isdead)
+        {
+            
+                if (pSS.pressE.WasPressedThisFrame())
+                {
+                    Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+                }
+        }
     }
 
 
     void OnControllerColliderHit(ControllerColliderHit colliderHit)
     {
         //print(colliderHit.gameObject);
-        //print(colliderHit.gameObject.tag);
+        print(colliderHit.gameObject.tag);
         
         if (colliderHit.gameObject.CompareTag("DEADLY"))
         {
            deathscreen.SetActive(true);
             pSS.enabled = false;
-            
+            isdead = true;
+
 
         } else if (colliderHit.gameObject.CompareTag("win"))
         {
