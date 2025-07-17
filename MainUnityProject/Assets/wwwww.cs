3595fb86f01e3a99006ffa93467c73f053c8e6e5
Programmering <teknisk+programmering-GDC2025-GR3@game.unf.dk>
2025-07-17 11:20:53 +0200

    awdawd

---
+++ MainUnityProject/Assets/wwwww.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class wwwww : MonoBehaviour
+{
+    
+    PlayerScript pSS;
+    public GameObject image;
+    public GameObject canvasObject;
+
+    attachplayer ap;
+    public GameObject trigger;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //image.SetActive(false);
+        ap = trigger.GetComponent<attachplayer>();
+        pSS = GameObject.FindWithTag("GameController").GetComponent<PlayerScript>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(0, 180, 0);
+        //(pSS.myState == PlayerScript.State.ATTACHED
+        
+        
+        
+        
+        
+    }
+    
+    void Update()
+    {
+        if (!ap)
+        {
+            return;
+        }
+
+        image.SetActive(true);
+        
+
+    }
+}
